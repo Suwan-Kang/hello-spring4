db8412c54e6ae0827c0efab77c8f8389398e9ec7
suwan <suwna.kang08@gmail.com>
2023-07-04 07:23:42 +0900

    회원 관리 예제 - 웹 MVC 개발(회원 웹 기능 - 조회)

--- src/main/java/com/project/hellospring3/controller/MemberController.java
+++ src/main/java/com/project/hellospring3/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.project.hellospring3.domain.Member;
 import com.project.hellospring3.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -28,6 +31,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 
